4b3711d670bc06292af1aef8f46b8da6ad5f8f3e
Víctor Peña Romero <vic.pero@icloud.com>
2021-03-04 07:36:47 -0600

    modify server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +6,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`)
     await sleep(5000);
   }
 }
